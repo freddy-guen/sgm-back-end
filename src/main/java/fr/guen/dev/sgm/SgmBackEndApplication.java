577f20cfb24429c6b191b8d30bc162b7709b5e0f
A749097 <freddy_guenengafo-biroboy@atos.net>
2023-05-17 15:05:31 +0200

    project structure

---
+++ src/main/java/fr/guen/dev/sgm/SgmBackEndApplication.java
@@ -0,0 +1,13 @@
+package fr.guen.dev.sgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgmBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgmBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/guen/dev/sgm/SgmBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.guen.dev.sgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SgmBackEndApplicationTests {
+
+   /* @Test
+    void contextLoads() {
+    }*/
+
+}
